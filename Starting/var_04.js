7eb3cfaf8c0019fd487420916f446e8088ffb191
kamleshlohani <87566930+kamleshlohani@users.noreply.github.com>
2024-01-27 00:28:50 +0000

    Lec-4

---
+++ Starting/var_04.js
@@ -0,0 +1,9 @@
+ac="Kamlesh"
+const a=23
+let ab="Lucknow"
+let b
+console.table([ac,a,ab,b])
+// group of variable you want to use as in a tabular form
+// prefer not to use 
+// var 
+// because it is having issue of block issue and the functional scope
