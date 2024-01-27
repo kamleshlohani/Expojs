16e0cce16577b14aee1cf5b55277f21569ad6ce0
kamleshlohani <87566930+kamleshlohani@users.noreply.github.com>
2024-01-27 01:30:57 +0000

    Learn all about datatypes and Echmasript

---
+++ Starting/datatypes_05.js
@@ -0,0 +1,28 @@
+// ECMAScript: It is a standard that all implementations of JavaScript must follow.
+// ECMAScript defines what a scripting language should look like, 
+//     while JavaScript is an actual implementation of that specification.
+
+// Scripting language: It is a programming language that employs a high-level construct to interpret and execute one command at a time
+
+"use strict"  //treat all JS code to newer Version
+alert(gf) //here it will throw an error because we are using node.js not the web browser where all the engine of JS is already present
+let a=34
+ const b=3
+let  isValid=true
+let d=null
+let e
+console.table([a,b,isValid]);
+
+// number=2^53
+// bigInt =store store number which is>2^53
+// string=""
+// boolean true/false
+// null=standalone value/empty value
+//     = undefined(kindda place holder for a value which is not defined yet (type = undefined))
+//symbol=unique
+//object
+console.log(typeof null);  //type is object
+console.log(typeof undefined); //type is undefined
+console.log(typeof true); //type is boolean
+console.log(typeof 64587);  //type is number
+console.log(typeof "Kamlesh"); //type is string
