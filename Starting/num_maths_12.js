18f150795160d0bc9660cf6d4661f02b83f8da7a
kamleshlohani <87566930+kamleshlohani@users.noreply.github.com>
2024-01-31 02:08:23 +0000

    completed nums and math

---
+++ Starting/num_maths_12.js
@@ -0,0 +1,41 @@
+// const score = 400
+// console.log(score);
+
+
+//another way of declaring  balance
+const balance = new Number(100)
+//  console.log(balance);
+
+//  console.log(balance.toString().length);
+ //tofixed():limits the number after the decimal
+//  console.log(balance.toFixed(2));
+
+ const otherNumber = 123.8966
+//  toPrecision():formats a number to a specified length
+ console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+//random():generate random number between 0 & 1
+// console.log(Math.random());
+
+//(Math.random()*10):means that we are having number from 0to 9 but b ut 10 for the 10 it should be multiply by 1 and this is not possible
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+//max-min:to addresss the range 
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
