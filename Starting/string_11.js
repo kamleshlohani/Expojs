ed47cb8cc815719e20332aeca45848f4a1f73380
kamleshlohani <87566930+kamleshlohani@users.noreply.github.com>
2024-01-31 01:30:25 +0000

    Concept of string

---
+++ Starting/string_11.js
@@ -0,0 +1,43 @@
+// let a="Kamlesh"
+// let b="Lohani"
+//old way
+// console.log
+//new way 
+// console.log(`Hello budddy my name is ${a} and my sir name is ${b}`)
+
+// We use ` ` to write complete string and will use ${} to inject variables in it.
+
+ const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+
+// exposes the value of the internal [[Prototype]] of an object
+//  console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+ console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+//slice():Select elements using negative values:
+
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());  
+// trim(): blank space erase kr deta hai
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
